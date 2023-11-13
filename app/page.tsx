8bf078dd7d26bd7e65febd20d7291e11eefe46ad
Amir <altghaderi@gmail.com>
2023-11-12 21:00:00 -0700

    Remove ' from text

--- app/page.tsx
+++ app/page.tsx
@@ -30,9 +30,9 @@ export default function Page() {
         </div>
         <Link
           href="/login"
-          className="flex items-center gap-4 self-center rounded-lg bg-black px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          className="flex items-center gap-4 self-center rounded-lg bg-gray-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-gray-800 md:text-base"
         >
-          <span>Let's Start</span>
+          <span>Let&lsquo;s Start</span>
         </Link>
 
         <div className="h-fit">
@@ -45,40 +45,6 @@ export default function Page() {
           />
         </div>
       </div>
-
-      {/* <div className="mx-auto flex max-w-7xl grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center items-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800`}>
-            <strong>Nexa Climate</strong>
-          </p>
-          <p
-            className={`${lusitana.className} text-emerald-800 font-bold text-2xl`}
-          >
-            <em>Go Green, Earn Green</em>
-          </p>
-          <p>
-            In an era prioritizing climate action, Nexa Climate leads in
-            sustainability, focusing on often-missed scope 3 emissions. Join us
-            in championing a future valuing progress and the planet. With Nexa
-            Climate, your business becomes a symbol of positive change.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
-        </div>
-        <div className="flex h-fit">
-          <Image
-            src="/landing.jpg"
-            width={500}
-            height={340}
-            className="hidden md:block rounded-md"
-            alt="landing"
-          />
-        </div>
-      </div> */}
     </main>
   );
 }
