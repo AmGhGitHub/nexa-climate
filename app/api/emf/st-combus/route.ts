c2ed06e1153dde1ffe1f8ccbc80b9b09deae309c
Amir <altghaderi@gmail.com>
2023-11-17 20:33:52 -0700

    Add revalidate to route

--- app/api/emf/st-combus/route.ts
+++ app/api/emf/st-combus/route.ts
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(req: NextRequest) {
   const emisCalculationBase = req.nextUrl.searchParams.get(
